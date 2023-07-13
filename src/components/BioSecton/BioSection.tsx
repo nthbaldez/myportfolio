44f6cc412482601b740935096348795357abf1e7
Nathan Baldez <nathanbsb2@hotmail.com>
2023-07-13 10:59:01 -0300

    feat: bio sections update

--- src/components/BioSecton/BioSection.tsx
+++ src/components/BioSecton/BioSection.tsx
@@ -15,7 +15,7 @@ export function BioSection() {
       <div id="profile">
         <h2>Sobre o Nathan Baldez</h2>
         <p>
-          Brasiliense, nascido em 1993.<br></br> Desde que conheci o mundo da programação, em meados de 2020, fiquei fascinado com a possibilidade de criar soluções para problemas reais do dia-a-dia por meio da programação. Sou formado em Análise e Desenvolvimento de Sistemas. Movido pelos constantes desafios que acarretam o Desenvolvimento Web, seja no Front-end como no Back-end. Além disso, apaixonado por música, sendo multi-instrumentista e artista nas horas vagas. Praticante de Calistenia e simpatizante de longos anos do skate. 
+          Brasiliense, nascido em 1993.<br></br> Desde que conheci o mundo da programação, em meados de 2020, venho criando soluções para o mundo real através do desenvolvimento de software. Sou formado em Análise e Desenvolvimento de Sistemas. Movido pelos constantes desafios que acarretam o Desenvolvimento Web, seja no Front-end como no Back-end. Além disso, apaixonado por música, sendo multi-instrumentista e artista nas horas vagas. Praticante de Calistenia e simpatizante de longos anos do skate. 
         </p>
       </div>
     </BioContainer>
